17e329138926d3721d03bf40b5456b4055b00c80
Nima Parsayi <nimaprs.ps@outlook.com>
2022-07-06 19:24:45 +0430

    add new method

--- RSSFeedReader.App/Controllers/MoviesController.cs
+++ RSSFeedReader.App/Controllers/MoviesController.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Timers;
 using CodeHollow.FeedReader;
 using Microsoft.AspNetCore.Mvc;
+using RSSFeedReader.DataLayer.Services;
+using RSSFeedReader.Robot.Bot;
 using RSSFeedReader.RobotCodes.Modules;
 
 namespace RSSFeedReader.App.Controllers
@@ -16,18 +18,21 @@ namespace RSSFeedReader.App.Controllers
             feedItems = FeedController.GetFeedItems(
                 "https://iptorrents.com/t.rss?u=1764728;tp=55ec80a8b063cabd26022bde0d0c268f;48;7;20;38;100;101;89;68;62;6;90;96;87;77;54").ToList();
 
+
             Timer timer = new Timer(300000);
             timer.Elapsed += Timer_Elapsed;
             timer.Enabled = true;
+            timer.Start();
+
 
         }
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        private async void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             var newItems = FeedController.GetFeedItems(
                 "https://iptorrents.com/t.rss?u=1764728;tp=55ec80a8b063cabd26022bde0d0c268f;48;7;20;38;100;101;89;68;62;6;90;96;87;77;54").ToList();
-
             newItems = newItems.Except(feedItems, new FeedComparer()).ToList();
+            await MessagesController.SendMessage(new MainRepository().GetActiveUsers(), newItems);
         }
     }
 }
